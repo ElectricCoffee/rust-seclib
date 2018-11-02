d83a75466d4289b1cd578e852cc55c1b379cf189
Niko Lepka <slench102+git@gmail.com>
2018-11-02 13:41:10 +0100

    more unit tests

--- src/tests.rs
+++ src/tests.rs
@@ -18,7 +18,30 @@ fn test_map() {
 }
 
 #[test]
-fn test_reveal_success() {
+fn test_and_then() {
+    fn f1(i: i32) -> Sec<High, i32> {
+        (i + 2).into()
+    }
+
+    let data: Sec<High, i32> = 4.into();
+    let result = data.and_then(f1);
+    let expected: Sec<High, i32> = 6.into();
+
+    assert_eq!(result, expected);
+
+    fn f2(i: i32) -> Sec<Low, i32> {
+        (i + 2).into()
+    }
+
+    let data: Sec<Low, i32> = 5.into();
+    let result = data.and_then(f2);
+    let expected: Sec<Low, i32> = 7.into();
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn test_reveal() {
     // testing high <= high
     let data: Sec<High, i32> = 12.into();
     let result = data.reveal(High);
@@ -30,4 +53,12 @@ fn test_reveal_success() {
     let result = data.reveal(High);
 
     assert_eq!(result, 13);
+
+    // testing low <= low
+    let data: Sec<Low, i32> = 14.into();
+    let result = data.reveal(Low);
+
+    assert_eq!(result, 14);
+
+    // NOTE: Last case cannot be tested, because it cannot compile. (which is the whole idea)
 }
